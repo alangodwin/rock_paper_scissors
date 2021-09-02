a8958788b4c11080007a39913ff1c44597d47d86
alangodwin <agodwin88@googlemail.com>
2021-09-02 14:10:06 +0100

    Added playRound function

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,17 @@
 function computerPlayer () {
     let action = ['rock', 'paper', 'scissors']
     return action[Math.floor(Math.random()*action.length)]
-
+}
+function playRound (computerSelection, playerSelection) {
+    if (computerSelection === playerSelection) {
+        return 'You both draw';
+    } else if (computerSelection === 'rock' && playerSelection === 'paper') {
+        return 'You win'
+    } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
+        return 'You win'
+    } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
+        return 'You win'
+    } else {
+        return 'You lose'
+    }
 }
