a07d7a845091b00244fab49fe971f234fb7e253f
alangodwin <agodwin88@googlemail.com>
2021-09-02 14:20:30 +0100

    changed text for winning, drawing and losing to be more specific

--- myscripts.js
+++ myscripts.js
@@ -4,14 +4,15 @@ function computerPlayer () {
 }
 function playRound (computerSelection, playerSelection) {
     if (computerSelection === playerSelection) {
-        return 'You both draw';
+        return `You both draw you cant beat ${computerSelection} with ${playerSelection}!`;
     } else if (computerSelection === 'rock' && playerSelection === 'paper') {
-        return 'You win'
+        return 'You win! Paper beats rock!';
     } else if (computerSelection === 'paper' && playerSelection === 'scissors') {
-        return 'You win'
+        return 'You win! Scissors beats paper!';
     } else if (computerSelection === 'scissors' && playerSelection === 'rock') {
-        return 'You win'
+        return 'You win! Rock beats Scissors';
     } else {
-        return 'You lose'
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
     }
 }
+//function game 
